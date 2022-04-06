fc23b65256fdb821eb2014ce467e3dad8a215c5f
DryginIvan <Vanyadrygin@gmail.com>
2022-04-06 18:07:31 +0300

    Initial commit

---
+++ src/geekbrains/com/Door.java
@@ -0,0 +1,7 @@
+package geekbrains.com;
+
+public class Door {
+
+    public int  weight;
+    public int height;
+}
---
+++ src/geekbrains/com/Main.java
@@ -0,0 +1,13 @@
+package geekbrains.com;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+
+        Home home = new Home();
+
+        Home home1 = new Home(1, 2, 3);
+        System.out.println(home1.height);
+    }
+}
